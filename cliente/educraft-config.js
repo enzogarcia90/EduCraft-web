2350043fb0961e045c78dfb93ad672f7935805e9
enzogarcia90 <enzogarcia90@users.noreply.github.com>
2026-09-05 18:50:30 +0200

    Publicar cliente: rebuild Turnstile fix

--- cliente/educraft-config.js
+++
@@ -1,5 +0,0 @@
-(function () {
-	window.EDUCRAFT_API_BASE_URL = "https://educraftes.duckdns.org";
-	window.EDUCRAFT_TURNSTILE_SITE_KEY = "0x4AAAAAAEjEKdA8hpsI8U9y";
-	window.EDUCRAFT_GOOGLE_CLIENT_ID = "875925687715-nv5bjtucaa2hghgq6co10d4t0ioqeij9.apps.googleusercontent.com";
-})();
